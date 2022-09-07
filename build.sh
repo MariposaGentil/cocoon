b7473d9f93a136dc7f222181992edfd805b344c2
Erik Tordera Bermejo <erik.tordera@qustodio.com>
2022-09-07 23:18:29 +0200

    feat: Added light and dark dynamic modes

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ then
     # Compile draft files and save to .site/drafts/
 	for draft_file in $draft_files
 	do
-		pandoc --template=./templates/custom-post-template.html5 --title-prefix="Cocoon" --css=/assets/css/styling.max.css -V lang=en -V highlighting-css= --mathjax \
+		pandoc --template=./templates/custom-post-template.html5 --title-prefix="Cocoon" --css=/assets/css/styling.max.css -H ./assets/js/cocoon.js.html -V lang=en -V highlighting-css= --mathjax \
 			--to=html5 ${draft_file} -o ./site/drafts/$(basename ${draft_file} .md).html
 	done
 else
@@ -64,7 +64,7 @@ done
 
 for root_file in $root_md_files
 do
-	pandoc --standalone --metadata pagetitle="Cocoon" --css=/assets/css/styling.max.css -V lang=en -V highlighting-css= --mathjax \
+	pandoc --standalone --metadata pagetitle="Cocoon" --css=/assets/css/styling.max.css -H ./assets/js/cocoon.js.html -V lang=en -V highlighting-css= --mathjax \
 		--to=html5 ${root_file} -o ./site/$(basename ${root_file} .md).html
 done
 
@@ -75,6 +75,6 @@ mkdir -p ./site/posts
 
 for post_file in $post_files
 do
-	pandoc --template=./templates/custom-post-template.html5 --title-prefix="Cocoon" --css=/assets/css/styling.max.css -V lang=en -V highlighting-css= --mathjax \
+	pandoc --template=./templates/custom-post-template.html5 --title-prefix="Cocoon" --css=/assets/css/styling.max.css -H ./assets/js/cocoon.js.html -V lang=en -V highlighting-css= --mathjax \
 		--to=html5 ${post_file} -o ./site/posts/$(basename ${post_file} .md).html
 done

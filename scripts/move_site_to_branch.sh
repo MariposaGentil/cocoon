0f51755c64f2d6e597266994ec74afe575f4bd26
Erik Tordera Bermejo <erik.tordera@qustodio.com>
2023-01-07 15:14:58 +0100

    test: test

--- scripts/move_site_to_branch.sh
+++ scripts/move_site_to_branch.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export branch=${ENV_REF}
-read -a split_branch <<< ${branch//'/'/' '}
 if [ "${branch}" = 'master' ]
 then
 	export dst_folder=''
 else
-	export dst_folder="subsites/${split_branch[-1]}"
+	export dst_folder="subsites/${branch}"
 fi
 echo "Copiying site to ${REPO_FOLDER}/${dst_folder}"
 mkdir  -p ${REPO_FOLDER}/${dst_folder}
--- scripts/push_to_github_pages.sh
+++ scripts/push_to_github_pages.sh
@@ -5,7 +5,7 @@ if [ $# -gt 0 ]
 then
 	if [ $1 = "--remove" ]
 	then
-	commit_prefix="${ENV_COMMIT_MSG} Removed subsite ${ENV_REF}"
+	commit_prefix="${ENV_COMMIT_MSG} Removed subsite ${branch}"
 	fi
 fi
 commit_msg="${commit_prefix} Automated commit generated from Gihub acions in remote repo"

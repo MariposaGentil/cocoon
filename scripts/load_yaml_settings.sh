c54480d559e48ad534d7b9aa2bdc302a0c02dae7
Erik Tordera Bermejo <erik.tordera@qustodio.com>
2023-10-05 10:53:46 +0200

    feat: Add settings files

---
+++ scripts/load_yaml_settings.sh
@@ -0,0 +1,22 @@
+export main_settings_path='./config/main.yaml'
+
+
+function parse_yaml {
+	 export filename=$(basename $1 .yaml)
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("-V %s_%s%s%s=\"%s\"\n", "'$filename'", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+parse_yaml $main_settings_path
